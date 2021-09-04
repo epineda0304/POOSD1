4d68261d2f6e74f427ced12035176de5f615854c
AndrewRodddd <62724891+AndrewRodddd@users.noreply.github.com>
2021-09-03 23:04:34 -0400

    Add files via upload

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+
+	$firstName = $inData["first name"];
+	$lastName = $inData["last name"];
+	$phoneNumber = $indata["(xxx)xxx-xxx"];
+	$email = $email["email@sample.com"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheCoders", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT into contacts (UserId, firstname, lastname, phoneNumber, email) VALUES(?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $phoneNumber, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
