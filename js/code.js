8ec01695a4153c1dc646f84810f640d4b25f519b
HelenDiaz <67114304+HelenDiaz@users.noreply.github.com>
2021-09-22 19:25:10 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -344,3 +344,26 @@ function showContact(numID)
 	document.getElementById("contactID").innerHTML = numID;
 
 }
+
+function mySearchFunction() 
+{
+  // Declare variables 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchContacts");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
